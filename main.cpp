b22fc541ec94f726d57e56fe8d6528786e124270
Slider <tornadodogca@gmail.com>
2026-07-21 19:20:23 -0700

    Remove comments in fastIO function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
 void fastIO() {
-    // Unties C++ streams from C streams for speed
     std::ios_base::sync_with_stdio(false);
-    // Unties cin from cout
     std::cin.tie(NULL); 
 }
