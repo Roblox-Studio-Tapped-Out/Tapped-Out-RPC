73dc1d372a8693cbc2603b3328f442af7c996b82
Slider <tornadodogca@gmail.com>
2026-07-21 19:19:48 -0700

    Implement fastIO function for improved I/O performance
    Added fastIO function to optimize input/output speed.

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void fastIO() {
+    // Unties C++ streams from C streams for speed
+    std::ios_base::sync_with_stdio(false);
+    // Unties cin from cout
+    std::cin.tie(NULL); 
+}
